2801c8da326d58563857d3fc61af96a7ebea60b4
Mason Wang <wangyachenmason@163.com>
2025-02-05 14:43:15 -0800

    serve frontend static page

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import entriesRouter from "./routes/entries.js";
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use("/api/entries/", entriesRouter);
+
+app.use(express.static("frontend"));
+app.use(express.json());
+app. use(express.urlencoded ({ extended: false }));
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
