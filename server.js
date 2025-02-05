cc5edf9380a05061f2260c61b44ec03168150a61
Mason Wang <wangyachenmason@163.com>
2025-02-05 14:10:01 -0800

    fix server.js ESlint Errors

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 const __dirname = path.resolve();
 
 // Serve static files from the "frontend" directory
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 // Serve the main HTML file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.listen(PORT, () => {
